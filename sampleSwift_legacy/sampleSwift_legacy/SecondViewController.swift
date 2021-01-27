9b8939e33e22429e55a52e9a8601b95b5b009f00
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-01-27 21:26:41 +0100

    feat(examples): cloned sampleSwift project to sampleSwift_legacy

---
+++ sampleSwift_legacy/sampleSwift_legacy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  sampleSwift
+//
+//  Created by Krystian on 27/01/2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
