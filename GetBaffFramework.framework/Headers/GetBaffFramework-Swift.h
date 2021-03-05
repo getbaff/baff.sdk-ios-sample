9d2d05f64cde21a0ccd62f4663f060f0cba7a206
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-03-05 14:35:30 +0100

    feat(sdk): updated GetBaffFramework.aar (3.6.0)

--- GetBaffFramework.framework/Headers/GetBaffFramework-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c32646ea393ca2fbb53cbe2984a7803adae91a4062fedef887c839fe7e8a60c4
-size 8909
---
+++ GetBaffFramework.framework/Headers/GetBaffFramework.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86baea1332f594141ffc4c52fa7f4102db859629e69985569cc3e40d20c1b6e4
+size 1516
